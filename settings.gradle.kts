d49ea59bc2dd358af149ce715d7a6ebde0d05e52
changukLee <ckddnr5527@gmail.com>
2024-07-29 13:19:17 +0900

    refactor: modify settings.gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,12 @@
 pluginManagement {
     repositories {
-        google()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         mavenCentral()
         gradlePluginPortal()
     }
@@ -15,5 +21,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "android-map-search"
+rootProject.name = "android-map-notification"
 include(":app")
