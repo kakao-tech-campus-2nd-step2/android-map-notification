72089a8a78ece768b06b3b0d4e25401b0f80b60e
akuby21 <akuby21@gmail.com>
2024-07-29 14:48:48 +0900

    refactor: implement databinding on ErrorActivity
    전주 피드백 사항인 ErrorActivity databinding 추가 적용

--- app/src/main/java/campus/tech/kakao/map/presenter/viewModel/ErrorViewModel.kt
+++ app/src/main/java/campus/tech/kakao/map/presenter/viewModel/ErrorViewModel.kt
@@ -1,8 +1,20 @@
 package campus.tech.kakao.map.presenter.viewModel
 
-import dagger.hilt.android.lifecycle.HiltViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import campus.tech.kakao.map.base.ErrorEnum
 
-@HiltViewModel
-class ErrorViewModel : viewModel{
+
+class ErrorViewModel : ViewModel() {
+    private val _type = MutableLiveData<ErrorEnum>()
+    val type: LiveData<ErrorEnum> = _type
+    private val _msg = MutableLiveData<String>()
+    val msg: LiveData<String> = _msg
+
+    fun setErrorData(type: ErrorEnum, msg: String) {
+        _type.value = type
+        _msg.value = msg
+    }
 
 }
