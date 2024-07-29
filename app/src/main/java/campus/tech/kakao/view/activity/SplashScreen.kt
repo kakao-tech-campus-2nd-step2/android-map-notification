b737393577a5eb2dbe4de7a90781919c7790336a
SHEEN <normengdie@pusan.ac.kr>
2024-07-29 17:58:31 +0900

    feat : go to Main when State is ON_SERVICE

--- app/src/main/java/campus/tech/kakao/view/activity/SplashScreen.kt
+++ app/src/main/java/campus/tech/kakao/view/activity/SplashScreen.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 import campus.tech.kakao.map.R
 import campus.tech.kakao.map.databinding.ActivitySplashScreenBinding
 import campus.tech.kakao.viewmodel.SplashViewModel
@@ -14,6 +16,7 @@ class SplashScreen : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashScreenBinding
     private lateinit var viewModel: SplashViewModel
+    private lateinit var remoteConfig: FirebaseRemoteConfig
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,10 +24,21 @@ class SplashScreen : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_splash_screen)
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
+
+        remoteConfig = FirebaseRemoteConfig.getInstance()
+        val configSettings = FirebaseRemoteConfigSettings.Builder()
+            .setMinimumFetchIntervalInSeconds(0)
+            .build()
+        remoteConfig.setConfigSettingsAsync(configSettings)
+
+        viewModel.fetchRemoteConfig(remoteConfig)
+
         viewModel.navigateToMain.observe(this, Observer { shouldNavigate ->
             if (shouldNavigate) {
                 startActivity(Intent(this, MainActivity::class.java))
                 finish()
+            } else {
+                binding.messageTextView.text = viewModel.serviceMessage.value
             }
         })
     }
--- app/src/main/java/campus/tech/kakao/viewmodel/SplashViewModel.kt
+++ app/src/main/java/campus/tech/kakao/viewmodel/SplashViewModel.kt
@@ -2,21 +2,52 @@ package campus.tech.kakao.viewmodel
 
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 
 class SplashViewModel : ViewModel() {
-    val imageUrl = MutableLiveData<String>().apply {
-        value = "@drawable/splash"
-    }
 
     private val _navigateToMain = MutableLiveData<Boolean>()
     val navigateToMain: LiveData<Boolean> get() = _navigateToMain
 
+    val serviceMessage = MutableLiveData<String>()
+    val imageUrl = MutableLiveData<String>()
+
     init {
-        Handler(Looper.getMainLooper()).postDelayed({
-            _navigateToMain.value = true
-        }, 2000) // 2초 후에 실행
+        _navigateToMain.value = false
+        imageUrl.value = "@drawable/splash"
+    }
+
+    fun fetchRemoteConfig(remoteConfig: FirebaseRemoteConfig) {
+        remoteConfig.fetchAndActivate()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val serviceState = remoteConfig.getString("serviceState")
+                    val serviceMessage = remoteConfig.getString("serviceMessage")
+                    Log.d("1234", "fetchRemoteConfig: $serviceState $serviceMessage")
+                    updateConfig(serviceState, serviceMessage)
+                } else {
+                    updateConfig(
+                        remoteConfig.getString("serviceState"),
+                        remoteConfig.getString("serviceMessage")
+                    )
+                }
+            }
+    }
+
+    private fun updateConfig(serviceState: String, serviceMessage: String) {
+        if (serviceState == "ON_SERVICE") {
+            Handler(Looper.getMainLooper()).postDelayed({
+                _navigateToMain.value = true
+            }, 2000)
+        } else {
+            this.serviceMessage.value = serviceMessage
+            Handler(Looper.getMainLooper()).postDelayed({
+                _navigateToMain.value = false
+            }, 2000)
+        }
     }
 }
