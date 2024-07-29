b0e6bea552b11989d9ddc56138b7bbf9a6ace70f
akuby21 <akuby21@gmail.com>
2024-07-29 20:50:50 +0900

    feat: implement SplashView
    splashview 구현

--- app/src/main/java/campus/tech/kakao/map/presenter/view/SplashActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/presenter/view/SplashActivity.kt
@@ -1,18 +1,11 @@
 package campus.tech.kakao.map.presenter.view
 
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
@@ -41,60 +34,10 @@ class SplashActivity : AppCompatActivity() {
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
 
-        askNotificationPermission()
-
-        viewModel.config.observe(this) {
-            if (it.serviceState == ServiceState.ON_SERVICE) {
-                Thread.sleep(1000)
-                startActivity(Intent(this, MapActivity::class.java))
+        viewModel.config.observe(this){
+            if(it.serviceState == ServiceState.ON_SERVICE){
+                startActivity(Intent(this,MapActivity::class.java))
             }
-        }
-
-
-    }
-
-    private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission(),
-    ) { isGranted: Boolean ->
-        if (isGranted) {
-            // FCM SDK (and your app) can post notifications.
-        } else {
-            // TODO: Inform user that that your app will not show notifications.
-        }
-    }
-
-    private fun askNotificationPermission() {
-        // This is only necessary for API level >= 33 (TIRAMISU)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
-                // FCM SDK (and your app) can post notifications.
-            } else if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
-                // 권한 요청 이유를 설명하는 UI를 표시
-                showNotificationPermissionDialog()
-            } else {
-                // Directly ask for the permission
-                requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
-            }
-        }
-    }
-
-    private fun showNotificationPermissionDialog() {
-        AlertDialog.Builder(this@SplashActivity).apply {
-            setTitle("타이틀 테스트")
-            setMessage(
-                String.format(
-                    "다양한 알림 소식을 받기 위해 권한을 허용하시겠어요?\n(알림 에서 %s의 알림 권한을 허용해주세요.)",
-                    getString(R.string.app_name)
-                )
-            )
-            setPositiveButton("네") { _, _ ->
-                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                val uri = Uri.fromParts("package", packageName, null)
-                intent.data = uri
-                startActivity(intent)
-            }
-            setNegativeButton("아뇨") { _, _ -> }
-            show()
         }
     }
 
