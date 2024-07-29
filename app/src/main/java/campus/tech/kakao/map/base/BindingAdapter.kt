b783d7a58992a76261d3853ddb8881dbb63f9988
akuby21 <akuby21@gmail.com>
2024-07-29 10:06:27 +0900

    init: load previous codes
    이전 코드 불러오기

--- app/src/main/java/campus/tech/kakao/map/base/BindingAdapter.kt
+++ app/src/main/java/campus/tech/kakao/map/base/BindingAdapter.kt
@@ -1,21 +1,9 @@
 package campus.tech.kakao.map.base
 
-import android.content.Intent
 import android.view.View
-import android.widget.TextView
 import androidx.databinding.BindingAdapter
-import campus.tech.kakao.map.R
-import campus.tech.kakao.map.presenter.view.MapActivity
 
 @BindingAdapter("visibleGone")
 fun setVisibleGone(view: View, visible: Boolean) {
     view.visibility = if (visible) View.VISIBLE else View.GONE
-}
-
-@BindingAdapter("errorText")
-fun setErrorTest(view: TextView, type: ErrorEnum){
-    view.text = when(type) {
-        ErrorEnum.MAP_LOAD_ERROR -> view.resources.getString(R.string.map_error)
-        else -> view.resources.getString(R.string.else_error)
-    }
 }
--- app/src/main/java/campus/tech/kakao/map/presenter/viewModel/ErrorViewModel.kt
+++ app/src/main/java/campus/tech/kakao/map/presenter/viewModel/ErrorViewModel.kt
@@ -1,20 +1,8 @@
 package campus.tech.kakao.map.presenter.viewModel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import campus.tech.kakao.map.base.ErrorEnum
+import dagger.hilt.android.lifecycle.HiltViewModel
 
-
-class ErrorViewModel : ViewModel() {
-    private val _type = MutableLiveData<ErrorEnum>()
-    val type: LiveData<ErrorEnum> = _type
-    private val _msg = MutableLiveData<String>()
-    val msg: LiveData<String> = _msg
-
-    fun setErrorData(type: ErrorEnum, msg: String) {
-        _type.value = type
-        _msg.value = msg
-    }
+@HiltViewModel
+class ErrorViewModel : viewModel{
 
 }
