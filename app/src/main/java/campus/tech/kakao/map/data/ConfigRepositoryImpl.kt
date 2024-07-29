9607e7bc2c36065e33ec7077c5103d368a555375
akuby21 <akuby21@gmail.com>
2024-07-29 20:51:53 +0900

    feat: implement loading serviceConfig in layer
    mvvm 패턴에 알맞게 데이터 레이어에 serviceConfig 불러오도록 구현

--- app/src/main/java/campus/tech/kakao/map/data/ConfigRepositoryImpl.kt
+++ app/src/main/java/campus/tech/kakao/map/data/ConfigRepositoryImpl.kt
@@ -4,10 +4,8 @@ import campus.tech.kakao.map.data.datasource.Local.Entity.toVO
 import campus.tech.kakao.map.data.datasource.Remote.ConfigService
 import campus.tech.kakao.map.domain.ConfigRepository
 import campus.tech.kakao.map.domain.vo.Config
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class ConfigRepositoryImpl @Inject constructor(private val configService: ConfigService) : ConfigRepository{
-    override suspend fun getConfig(): Config = withContext(Dispatchers.IO){ configService.getConfig().toVO() }
+    override fun getConfig(): Config = configService.getConfig().toVO()
 }
