556b7e4b3d0d957fdedd3fcd2075e8d3524deac9
akuby21 <akuby21@gmail.com>
2024-07-29 14:48:48 +0900

    refactor: implement databinding on ErrorActivity
    전주 피드백 사항인 ErrorActivity databinding 추가 적용

--- app/src/main/java/campus/tech/kakao/map/presenter/view/ErrorActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/presenter/view/ErrorActivity.kt
@@ -5,60 +5,55 @@ import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import campus.tech.kakao.map.base.ErrorEnum
 import campus.tech.kakao.map.R
 import campus.tech.kakao.map.databinding.ActivityErrorBinding
+import campus.tech.kakao.map.presenter.viewModel.ErrorViewModel
+import campus.tech.kakao.map.presenter.viewModel.MapViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import java.io.Serializable
 
 
 @AndroidEntryPoint
 class ErrorActivity : AppCompatActivity() {
+    private val viewModel: ErrorViewModel by viewModels()
     private lateinit var binding: ActivityErrorBinding
-    private lateinit var type : ErrorEnum
-    private lateinit var msg : String
+    private lateinit var type: ErrorEnum
+    private lateinit var msg: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_error)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_error)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
 
         getExtras()
-        settingText()
         setRetryListener()
     }
 
-    private fun getExtras(){
-        type = intent.intentSerializable(ERROR_TYPE,ErrorEnum :: class.java) ?: ErrorEnum.ELSE
+    private fun getExtras() {
+        type = intent.intentSerializable(ERROR_TYPE, ErrorEnum::class.java) ?: ErrorEnum.ELSE
         msg = intent.extras?.getString(ERROR_MSG) ?: ERROR_UNKNOWN
+        viewModel.setErrorData(type, msg)
     }
 
-    private fun settingText(){
-        binding.errorText.text = when(type){
-            ErrorEnum.MAP_LOAD_ERROR -> getString(R.string.map_error)
-            else -> getString(R.string.else_error)
-        }
-
-        binding.errorMsg.text = msg
-    }
-
-    private fun setRetryListener(){
-        binding.retryBtn.setOnClickListener{
-            val intent = when(type){
-                ErrorEnum.MAP_LOAD_ERROR -> Intent(this,MapActivity::class.java)
+    private fun setRetryListener() {
+        binding.retryBtn.setOnClickListener {
+            val intent = when (type) {
+                ErrorEnum.MAP_LOAD_ERROR -> Intent(this, MapActivity::class.java)
                 else -> null
             }
 
-            intent?.let{
-                startActivity(it)
-            }
+            intent?.let { startActivity(it) }
         }
     }
 
-    private fun <T: Serializable> Intent.intentSerializable(key: String, clazz: Class<T>): T? {
+    private fun <T : Serializable> Intent.intentSerializable(key: String, clazz: Class<T>): T? {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             this.getSerializableExtra(key, clazz)
         } else {
@@ -66,7 +61,7 @@ class ErrorActivity : AppCompatActivity() {
         }
     }
 
-    companion object{
+    companion object {
         const val ERROR_TYPE = "type"
         const val ERROR_MSG = "msg"
         const val ERROR_UNKNOWN = "unknown"
