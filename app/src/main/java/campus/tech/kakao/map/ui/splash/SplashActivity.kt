eb258ee3766d311f3ac932669849ffc90508c517
Jeong Suhyeon <jsh00325@naver.com>
2024-07-30 17:18:52 +0900

    feat: Bind Splash View and Splash ViewModel

--- app/src/main/java/campus/tech/kakao/map/ui/splash/SplashActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/ui/splash/SplashActivity.kt
@@ -1,11 +1,14 @@
 package campus.tech.kakao.map.ui.splash
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import campus.tech.kakao.map.R
 import campus.tech.kakao.map.databinding.ActivitySplashBinding
+import campus.tech.kakao.map.ui.map.MapActivity
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -20,7 +23,16 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
         binding.lifecycleOwner = this
+        binding.viewModel = viewModel
 
-
+        viewModel.serviceState.observe(this) { serviceState ->
+            if (serviceState == "ON_SERVICE") {
+                val intent = Intent(this@SplashActivity, MapActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                binding.splashInformationTextView.isVisible = true
+            }
+        }
     }
 }
