76f1532d095eb9f5086790259cb219859d906ae9
SHEEN <normengdie@pusan.ac.kr>
2024-07-30 19:27:51 +0900

    feat : diffrence FCM when FORE OR BACK

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,12 @@ plugins {
     id("org.jlleitschuh.gradle.ktlint") version "12.1.0" apply false
     id("com.google.dagger.hilt.android") version "2.48.1" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
+
+}
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.3.10")
+    }
 }
 
 allprojects {
