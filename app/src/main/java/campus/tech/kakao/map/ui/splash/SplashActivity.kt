d8f7c3b3094ef4cc94cf87123d118330a33be3f7
Jeong Suhyeon <jsh00325@naver.com>
2024-07-30 15:06:29 +0900

    feat: Add Custom Splash Screen Activity & Layout

---
+++ app/src/main/java/campus/tech/kakao/map/ui/splash/SplashActivity.kt
@@ -0,0 +1,21 @@
+package campus.tech.kakao.map.ui.splash
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import campus.tech.kakao.map.R
+import campus.tech.kakao.map.databinding.ActivitySplashBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
+        binding.lifecycleOwner = this
+
+
+    }
+}
