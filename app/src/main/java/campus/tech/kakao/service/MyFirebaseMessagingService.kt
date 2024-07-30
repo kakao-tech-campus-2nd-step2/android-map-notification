676e94a330638a3ba46907bf0f50ead33572d6e4
SHEEN <normengdie@pusan.ac.kr>
2024-07-30 19:33:11 +0900

    feat : Can not click notification When Foreground

--- app/src/main/java/campus/tech/kakao/service/MyFirebaseMessagingService.kt
+++ app/src/main/java/campus/tech/kakao/service/MyFirebaseMessagingService.kt
@@ -26,7 +26,10 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         super.onMessageReceived(remoteMessage)
 
         if (isAppInForeground()) {
-            sendNotification(CUSTOM_TITLE, CUSTOM_BODY)
+            sendNotification(CUSTOM_TITLE, CUSTOM_BODY, false)
+        } else {
+            sendNotification(remoteMessage.notification?.title ?: "Default Title",
+                remoteMessage.notification?.body ?: "Default Body", true)
         }
     }
 
@@ -43,11 +46,18 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         return false
     }
 
-    private fun sendNotification(title: String, messageBody: String) {
+    private fun sendNotification(title: String, messageBody: String, enableClick: Boolean) {
         val intent = Intent(this, SplashScreen::class.java).apply {
             addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            putExtra("title", title)
+            putExtra("messageBody", messageBody)
         }
-        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE)
+
+        val pendingIntent: PendingIntent? = if (enableClick) {
+            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE)
+        } else {
+            null
+        }
 
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
         val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
@@ -56,11 +66,13 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
             .setContentText(messageBody)
             .setAutoCancel(true)
             .setSound(defaultSoundUri)
-            .setContentIntent(pendingIntent)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
 
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (pendingIntent != null) {
+            notificationBuilder.setContentIntent(pendingIntent)
+        }
 
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
             return
