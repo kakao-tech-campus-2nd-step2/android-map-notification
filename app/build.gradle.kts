567e9e5d8d64a82d0e71b34880a70e3d915c00cc
Kjamm <happyminig@gmail.com>
2024-07-31 22:09:32 +0900

    충남대 Android_김재민 6주차 과제 STEP 1 (#31)
    * docs : Update README.md
    
    * feat : gradle 업데이트
    
    * feat : 5주차 코드 병합
    
    * refactor : 누락된 부분 수정
    
    * docs : README.md 업데이트
    
    * feat : Splash 화면 구성 및 매개변수 값에 따라 진입 결정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
+    id("com.google.gms.google-services")
     id("com.google.dagger.hilt.android")
 }
 
