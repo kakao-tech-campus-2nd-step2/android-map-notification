7cf9b21c543cacffe0c98a859b90cbee4aef5e5c
anyooin <100949208+anyooin@users.noreply.github.com>
2024-07-31 22:29:33 +0900

    feat : add LoadingActivity

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 - 매개변수 이름:serviceState, serviceMessage
 - 매개변수 serviceState 값이 ON_SERVICE일 때만 초기 진입 화면이 지도 화면으로 넘어간다.
 - 매개변수 serviceState 값이 ON_SERVICE이 아닌 경우에는 serviceMessage 값을 초기 진입 화면 하단에 표시하고 지도 화면으로 진입하지 않는다.
-- 
+
 ## 구현할 기능
 - 초기 진입 화면 만들기
 - Firebase의 Remote Config를 설정
---
+++ app/src/main/java/campus/tech/kakao/map/activity/LoadingActivity.kt
@@ -0,0 +1,12 @@
+package campus.tech.kakao.map.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import campus.tech.kakao.map.R
+
+class LoadingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+    }
+}
