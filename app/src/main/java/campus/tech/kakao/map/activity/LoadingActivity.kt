6c20187e087b802dca1ff3ce4e785a258ef366df
anyooin <100949208+anyooin@users.noreply.github.com>
2024-08-01 03:06:23 +0900

    feat : use MVVM

--- app/src/main/java/campus/tech/kakao/map/activity/LoadingActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/activity/LoadingActivity.kt
@@ -3,18 +3,26 @@ package campus.tech.kakao.map.activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import campus.tech.kakao.map.LoadingViewModel
 import campus.tech.kakao.map.R
+import campus.tech.kakao.map.Room.MapItemViewModel
 import campus.tech.kakao.map.databinding.ActivityLoadingBinding
 import campus.tech.kakao.map.databinding.ActivitySearchBinding
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.google.firebase.remoteconfig.remoteConfigSettings
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoadingActivity : AppCompatActivity() {
+    private val loadingViewModel: LoadingViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loading)
@@ -22,29 +30,19 @@ class LoadingActivity : AppCompatActivity() {
         val binding: ActivityLoadingBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_loading)
 
-        val remoteConfig: FirebaseRemoteConfig = Firebase.remoteConfig
-        val configSettings = remoteConfigSettings {
-            minimumFetchIntervalInSeconds = 0 // 개발용
-        }
-        remoteConfig.setConfigSettingsAsync(configSettings)
-
-        remoteConfig.setDefaultsAsync(R.xml.remote_config_defaults)
-
-        remoteConfig.fetchAndActivate()
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    val state = remoteConfig.getString("serviceState")
-                    Log.d("james", "state: $state")
-                    if(state == "ON_SERVICE") {
-                        val intent = Intent(this, MapActivity::class.java)
-                        //intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                        startActivity(intent)
-                        finish()
-                    } else {
-                        binding.loadingText.text = remoteConfig.getString("serviceMessage")
-                    }
-
-                }
+        loadingViewModel.serviceState.observe(this) {
+            Log.d("uin", ""+ loadingViewModel.serviceState.value)
+            Log.d("uin", ""+ loadingViewModel.serviceMessage.value)
+            if(loadingViewModel.serviceState.value == "ON_SERVICE") {
+                val intent = Intent(this, MapActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Log.d("uin", "!!")
+                binding.loadingText.text = loadingViewModel.serviceMessage.value
+                Log.d("uin", "!!!")
             }
+        }
+
     }
 }
