7c57b7665c8657efc7c5c5dae031c063e93befbf
Jeong Suhyeon <jsh00325@naver.com>
2024-08-01 23:44:51 +0900

    feat: Add Intent at Custom Notification

--- app/src/main/java/campus/tech/kakao/map/MapFirebaseMessagingService.kt
+++ app/src/main/java/campus/tech/kakao/map/MapFirebaseMessagingService.kt
@@ -2,8 +2,11 @@ package campus.tech.kakao.map
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import campus.tech.kakao.map.ui.splash.SplashActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -36,11 +39,19 @@ class MapFirebaseMessagingService : FirebaseMessagingService() {
     private fun sendNotification(title: String, message: String) {
         createNotificationChannel()
 
+        val intent = Intent(this, SplashActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent =
+            PendingIntent.getActivities(this, 0, arrayOf(intent), PendingIntent.FLAG_IMMUTABLE)
+
         val notification = NotificationCompat.Builder(this, CHANNEL_ID)
             .setSmallIcon(R.drawable.map_notification_icon)
             .setColor(getColor(R.color.notificationBackground))
             .setContentTitle(title)
             .setContentText(message)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
             .build()
 
         val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
