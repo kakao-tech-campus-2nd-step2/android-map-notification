74ea599b1244f2ca499b4bf82a42f75e2ce73a7c
arieum <lar713@naver.com>
2024-08-01 17:43:17 +0900

    feat: Add Foreground_Custom_Notification

--- app/src/main/java/campus/tech/kakao/map/repository/WelcomeRepository.kt
+++ app/src/main/java/campus/tech/kakao/map/repository/WelcomeRepository.kt
@@ -1,4 +1,55 @@
 package campus.tech.kakao.map.repository
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import campus.tech.kakao.map.R
+import campus.tech.kakao.map.view.WelcomeActivity
+
 class WelcomeRepository {
+    fun sendForegroundNotification(context: Context) {
+        val intent = Intent(context, WelcomeActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        val notification = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("KAKAO MAP")
+            .setContentText("IN FOREGROUND, THIS APP IS RUNNING ...!")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .build()
+
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(NOTIFICATION_ID, notification)
+    }
+    fun createNotificationChannel(context: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = CHANNEL_ID
+            val channelName = CHANNEL_NAME
+            val channelDescription = CHANNEL_DESCRIPTION
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+
+            val channel = NotificationChannel(channelId, channelName, importance).apply {
+                description = channelDescription
+            }
+
+            val notificationManager: NotificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    companion object {
+        private const val NOTIFICATION_ID = 222222
+        private const val CHANNEL_ID = "foreground_channel"
+        private const val CHANNEL_NAME = "foreground_channel"
+        private const val CHANNEL_DESCRIPTION = "This is foreground_channel"
+    }
 }
