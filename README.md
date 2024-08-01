25c8bc43d726727ea458db51d1adb0732f565055
nJiyeon <150491973+nJiyeon@users.noreply.github.com>
2024-08-01 21:47:31 +0900

    경북대 Android 남지연 6주차 과제 Step0 (#40)
    * update README.md
    
    * android-map-refactoring 코드 가져오기
    
    * keywordViewModel을 public으로 바꾸기

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # android-map-notification
+## 이전 미션 코드 가져오기
+- 리팩터링 코드를 옮겨 온다.
---
+++ app/src/main/java/campus/tech/kakao/MyApplication.kt
@@ -0,0 +1,14 @@
+package campus.tech.kakao
+
+import android.app.Application
+import com.kakao.vectormap.KakaoMapSdk
+import campus.tech.kakao.map.R
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        KakaoMapSdk.init(this, getString(R.string.KAKAO_API_KEY))
+    }
+}
