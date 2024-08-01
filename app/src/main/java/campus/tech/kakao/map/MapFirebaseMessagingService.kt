ccab267164a91a2ef476548d5c720c841efd6bf4
anyooin <100949208+anyooin@users.noreply.github.com>
2024-08-02 01:48:03 +0900

    feat : foreground Message

--- app/src/main/java/campus/tech/kakao/map/MapFirebaseMessagingService.kt
+++ app/src/main/java/campus/tech/kakao/map/MapFirebaseMessagingService.kt
@@ -1,21 +1,73 @@
 package campus.tech.kakao.map
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import campus.tech.kakao.map.activity.LoadingActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class MapFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        Log.d("uin", "From: ${remoteMessage.from}")
+        super.onMessageReceived(remoteMessage)
 
-        remoteMessage.notification?.let {
-            Log.d("uin", "Message Notification Body: ${it.body}")
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val channel = createNotificationChannel()
+        notificationManager.createNotificationChannel(channel)
+        val intent = Intent(this, LoadingActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         }
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        val builder = NotificationCompat.Builder(
+            this,
+            CHANNEL_ID
+        )
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("[중요] 포그라운드 알림")
+            .setContentText("앱이 실행 중입니다.")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText("앱이 실행 중일 때는 포그라운드 알림이 발생합니다.")
+            )
+            .setAutoCancel(true)
+
+        notificationManager.notify(NOTIFICATION_ID, builder.build())
+    }
+
+    private fun createNotificationChannel(): NotificationChannel {
+        val descriptionText = "descriptionText"
+        val channel = NotificationChannel(
+            CHANNEL_ID,
+            CHANNEL_NAME,
+            NotificationManager.IMPORTANCE_DEFAULT
+        ).apply {
+            description = descriptionText
+        }
+        return channel
+        //notificationManager.createNotificationChannel(channel)
+    }
+
+    companion object {
+        private const val NOTIFICATION_ID = 222222
+        private const val CHANNEL_ID = "main_default_channel"
+        private const val CHANNEL_NAME = "main channelName"
     }
 
 }
