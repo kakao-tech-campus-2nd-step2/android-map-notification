30a8e95d6d2ae1e47371a25f25e532a169538434
Jeong Suhyeon <jsh00325@naver.com>
2024-08-01 16:38:47 +0900

    feat: Add Custom Notification at onMessageReceived

--- app/src/main/java/campus/tech/kakao/map/MapFirebaseMessagingService.kt
+++ app/src/main/java/campus/tech/kakao/map/MapFirebaseMessagingService.kt
@@ -1,6 +1,9 @@
 package campus.tech.kakao.map
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -13,11 +16,39 @@ class MapFirebaseMessagingService : FirebaseMessagingService() {
         Log.d(TAG, "From: ${message.from}")
 
         message.notification?.let {
-            Log.d(TAG, "Message Notification Body: ${it.body}")
+            sendNotification("[foreground]" + (it.title ?: ""), it.body ?: "")
         }
+    }
+
+    private fun createNotificationChannel() {
+        val notificationChannel = NotificationChannel(
+            CHANNEL_ID,
+            getString(R.string.notificationChannelName),
+            NotificationManager.IMPORTANCE_DEFAULT
+        ).apply {
+            description = getString(R.string.notificationChannelDescription)
+        }
+
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(notificationChannel)
+    }
+
+    private fun sendNotification(title: String, message: String) {
+        createNotificationChannel()
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.map_notification_icon)
+            .setColor(getColor(R.color.notificationBackground))
+            .setContentTitle(title)
+            .setContentText(message)
+            .build()
+
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(0, notification)
     }
 
     companion object {
         private const val TAG = "MapFirebaseMessagingService"
+        private const val CHANNEL_ID = "custom_notification_channel_id"
     }
 }
