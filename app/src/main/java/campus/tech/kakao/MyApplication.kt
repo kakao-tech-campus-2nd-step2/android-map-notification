1bc962576d39fa7df4f3e23cc6f665789065e1d2
유지예 <jiyeyu@192.168.nate.com>
2024-08-01 10:30:21 +0900

    feat: remote config

--- app/src/main/java/campus/tech/kakao/MyApplication.kt
+++ app/src/main/java/campus/tech/kakao/MyApplication.kt
@@ -2,6 +2,8 @@ package campus.tech.kakao
 
 import android.app.Application
 import campus.tech.kakao.map.R
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 import com.kakao.vectormap.KakaoMapSdk
 import dagger.hilt.android.HiltAndroidApp
 
@@ -11,5 +13,15 @@ class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         KakaoMapSdk.init(this, getString(R.string.kakao_api_key))
+        initFirebaseRemoteConfig()
+    }
+
+    private fun initFirebaseRemoteConfig() {
+        val remoteConfig = FirebaseRemoteConfig.getInstance()
+        val configSettings = FirebaseRemoteConfigSettings.Builder()
+            .setMinimumFetchIntervalInSeconds(1800) //30분마다 가져오기
+            .build()
+        remoteConfig.setConfigSettingsAsync(configSettings)
+        remoteConfig.setDefaultsAsync(R.xml.remote_config_defaults)
     }
 }
