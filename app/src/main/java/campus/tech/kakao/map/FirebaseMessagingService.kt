93e0912c55e66cafdb77c12715c6b94b102878ec
fiveJinw <fiveJinw1124@gmail.com>
2024-08-02 15:55:34 +0900

    feat : Add feature that receive notification

--- app/src/main/java/campus/tech/kakao/map/FirebaseMessagingService.kt
+++ app/src/main/java/campus/tech/kakao/map/FirebaseMessagingService.kt
@@ -1,7 +1,63 @@
 package campus.tech.kakao.map
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import campus.tech.kakao.map.view.SplashActivity
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class AppFirebaseMessagingService : FirebaseMessagingService() {
+    private lateinit var notificationManager: NotificationManager
 
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d("testtt", "From: ${remoteMessage.from}")
+
+        remoteMessage.notification?.let {
+            Log.d("testtt", "Message Notification Body: ${it.body}")
+        }
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        createNotificationChannel()
+        val intent = Intent(this, SplashActivity::class.java).apply { // 클릭하면 출력될 Activity
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        val builder = NotificationCompat.Builder(
+            this,
+            CHANNEL_ID
+        )
+            .setSmallIcon(R.drawable.ic_launcher_foreground) // 필수
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        notificationManager.notify(NOTIFICATION_ID, builder.build())
+
+    }
+
+    companion object {
+        private const val NOTIFICATION_ID = 1
+        private const val CHANNEL_ID = "main_default_channel"
+        private const val CHANNEL_NAME = "main channelName"
+    }
+    private fun createNotificationChannel() {
+        val descriptionText = "알림 테스트"
+        val channel = NotificationChannel(
+            CHANNEL_ID,
+            CHANNEL_NAME,
+            NotificationManager.IMPORTANCE_DEFAULT
+        ).apply {
+            description = descriptionText
+        }
+        notificationManager.createNotificationChannel(channel)
+    }
 }
--- app/src/main/java/campus/tech/kakao/map/view/SplashActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/view/SplashActivity.kt
@@ -1,13 +1,20 @@
 package campus.tech.kakao.map.view
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.provider.Settings
 import android.util.Log
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -19,7 +26,9 @@ import campus.tech.kakao.map.viewmodel.SearchActivityViewModel
 import campus.tech.kakao.map.viewmodel.SplashActivityViewModel
 import campus.tech.kakao.map.viewmodel.SplashUIState
 import campus.tech.kakao.map.viewmodel.UiState
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.Firebase
+import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.get
 import com.google.firebase.remoteconfig.remoteConfig
@@ -34,9 +43,14 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        askNotificationPermission()
+
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initObserve()
+
+        getFCMToken()
+
 
     }
 
@@ -74,7 +88,66 @@ class SplashActivity : AppCompatActivity() {
         }
     }
 
-    
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // FCM SDK (and your app) can post notifications.
+        } else {
+            // TODO: Inform user that that your app will not show notifications.
+        }
+    }
+
+    private fun askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.POST_NOTIFICATIONS
+                ) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                // FCM SDK (and your app) can post notifications.
+            } else if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
+                // 권한 요청 이유를 설명하는 UI를 표시
+                showNotificationPermissionDialog()
+            } else {
+                // Directly ask for the permission
+                requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+    }
+
+    private fun showNotificationPermissionDialog() {
+        AlertDialog.Builder(this@SplashActivity).apply {
+            setTitle("권한 확인")
+            setMessage(
+                String.format(
+                    "다양한 알림 소식을 받기 위해 권한을 허용하시겠어요?\n(알림 에서 %s의 알림 권한을 허용해주세요.)",
+                    getString(R.string.app_name)
+                )
+            )
+            setPositiveButton("yes") { _, _ ->
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package", packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            }
+            setNegativeButton("no") { _, _ -> }
+            show()
+        }
+    }
+
+    fun getFCMToken(){
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                return@OnCompleteListener
+            }
+            Log.d("testtt", task.result.toString())
+
+        })
+    }
+
 
 }
 
