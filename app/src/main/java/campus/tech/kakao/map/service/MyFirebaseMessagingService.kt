d1a1af74b685b197c76d3745050c8b8f2b94e3b2
Kjamm <happyminig@gmail.com>
2024-08-03 08:15:14 +0900

    충남대 Android_김재민 6주차 과제 STEP 2 (#51)
    * docs : Update README.md
    
    * feat : gradle 업데이트
    
    * feat : 5주차 코드 병합
    
    * refactor : 누락된 부분 수정
    
    * docs : README.md 업데이트
    
    * feat : Splash 화면 구성 및 매개변수 값에 따라 진입 결정
    
    * docs : README.md 업데이트
    
    * feat : 백그라운드, 포그라운드 상태에 따라 Notification 발생
    
    * refactor : STEP 1 피드백 반영(상수 지정)

---
+++ app/src/main/java/campus/tech/kakao/map/service/MyFirebaseMessagingService.kt
@@ -0,0 +1,87 @@
+package campus.tech.kakao.map.service
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import campus.tech.kakao.map.R
+import campus.tech.kakao.map.ui.SplashActivity
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+
+        remoteMessage.notification?.let {
+            showNotification(it.title, it.body)
+        }
+
+        if (remoteMessage.data.isNotEmpty()) {
+            showCustomNotification(remoteMessage.data["title"], remoteMessage.data["body"])
+        }
+    }
+
+    private fun showNotification(title: String?, message: String?) {
+        val intent = Intent(this, SplashActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, intent, PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val channelId = getString(R.string.default_notification_channel_id)
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.marker)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId, "Channel human readable title", NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+
+    private fun showCustomNotification(title: String?, message: String?) {
+        val intent = Intent(this, SplashActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, intent, PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val channelId = getString(R.string.default_notification_channel_id)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.marker)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId, "Channel human readable title", NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+}
