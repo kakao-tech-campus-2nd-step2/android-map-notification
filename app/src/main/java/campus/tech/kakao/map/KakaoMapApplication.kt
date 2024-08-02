3eb770e0e35116b0d72bfad9afa1bffef12011b7
jooiss <jusong513@pusan.ac.kr>
2024-08-02 10:45:14 +0900

    feat: Enable foreground notifications for FCM

--- app/src/main/java/campus/tech/kakao/map/KakaoMapApplication.kt
+++ app/src/main/java/campus/tech/kakao/map/KakaoMapApplication.kt
@@ -1,14 +1,33 @@
 package campus.tech.kakao.map
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import campus.tech.kakao.map.BuildConfig
 import com.kakao.vectormap.KakaoMapSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class KakaoMapApplication : Application() {
+
     override fun onCreate() {
         super.onCreate()
+        createNotificationChannel()
         KakaoMapSdk.init(this, BuildConfig.KAKAO_API_KEY)
+    }
+
+    private fun createNotificationChannel() {
+        val descriptionText = getString(R.string.channel_description)
+        val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT).apply {
+            description = descriptionText
+        }
+        val notificationManager: NotificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    companion object {
+        private const val NOTIFICATION_ID = 222222
+        private const val CHANNEL_ID = "main_default_channel"
+        private const val CHANNEL_NAME = "main channelName"
     }
 }
--- app/src/main/java/campus/tech/kakao/map/MapFirebaseMessagingService.kt
+++ app/src/main/java/campus/tech/kakao/map/MapFirebaseMessagingService.kt
@@ -1,16 +1,25 @@
 package campus.tech.kakao.map
 
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import campus.tech.kakao.map.ui.SplashActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class MapFirebaseMessagingService : FirebaseMessagingService() {
-
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         Log.d("notification", "From: ${remoteMessage.from}")
 
         remoteMessage.notification?.let {
             Log.d("notification", "Message Notification Body: ${it.body}")
+        }
+
+        if (remoteMessage.notification != null) {
+            showNotification()
         }
     }
 
@@ -21,6 +30,38 @@ class MapFirebaseMessagingService : FirebaseMessagingService() {
 
     private fun sendRegistrationToServer(token: String?) {
         Log.d("notification", "sendRegistrationTokenToServer($token)")
+    }
+
+    private fun showNotification() {
+        val intent = Intent(this, SplashActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("[중요] 포그라운드 알림")
+            .setContentText("앱이 실행 중입니다.")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText("앱이 실행 중일 때는 포그라운드 알림이 발생합니다.")
+            )
+            .setAutoCancel(true)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(NOTIFICATION_ID, builder.build())
+    }
+
+    companion object {
+        private const val NOTIFICATION_ID = 222222
+        private const val CHANNEL_ID = "main_default_channel"
+        private const val CHANNEL_NAME = "main channelName"
     }
 
 }
