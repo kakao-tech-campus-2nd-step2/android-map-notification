3ff02b54072dcae78de1e1ee1e0fc68ba687d134
arieum <lar713@naver.com>
2024-08-02 23:37:43 +0900

    refactor: remove img entity in place

--- app/src/main/java/campus/tech/kakao/map/data/db/entity/Place.kt
+++ app/src/main/java/campus/tech/kakao/map/data/db/entity/Place.kt
@@ -1,13 +1,11 @@
 package campus.tech.kakao.map.data.db.entity
 
-import androidx.annotation.DrawableRes
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "research")
 data class Place (
     @PrimaryKey(autoGenerate = true) val id: Int? = null,
-    @DrawableRes val img: Int,
     val name: String,
     val location: String,
     val category: String,
--- app/src/main/java/campus/tech/kakao/map/repository/PlaceRepository.kt
+++ app/src/main/java/campus/tech/kakao/map/repository/PlaceRepository.kt
@@ -3,7 +3,6 @@ package campus.tech.kakao.map.repository
 import android.content.SharedPreferences
 import android.util.Log
 import campus.tech.kakao.map.BuildConfig
-import campus.tech.kakao.map.R
 import campus.tech.kakao.map.data.db.entity.Place
 import campus.tech.kakao.map.data.remote.api.KakaoApiService
 import javax.inject.Inject
@@ -20,7 +19,6 @@ class PlaceRepository @Inject constructor(
             val documentList = response.documents ?: emptyList()
             documentList.map {
                 Place(
-                    img = R.drawable.location,
                     name = it.placeName,
                     location = it.addressName,
                     category = it.categoryGroupName,
