1a6f5b7960fe73149ded6952961789d624dd3424
seyoung34 <jupi0304@naver.com>
2024-08-02 16:44:40 +0900

    splash screen 구현

--- app/src/main/java/campus/tech/kakao/map/view/EnterActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/view/EnterActivity.kt
@@ -5,52 +5,46 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import campus.tech.kakao.map.R
 import campus.tech.kakao.map.databinding.ActivityEnterBinding
+import campus.tech.kakao.map.viewmodel.MyViewModel
 import com.google.firebase.Firebase
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.remoteConfig
 import com.google.firebase.remoteconfig.remoteConfigSettings
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class EnterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEnterBinding
+    private val viewModel: MyViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_enter)
+        binding.viewModel = viewModel
 
-        val remoteConfig: FirebaseRemoteConfig = Firebase.remoteConfig
-        val configSettings = remoteConfigSettings {
-            minimumFetchIntervalInSeconds = 0 // 개발용
-        }
-        remoteConfig.setConfigSettingsAsync(configSettings)
+        viewModel.fetchRemoteConfig()
 
-
-        remoteConfig.fetchAndActivate()
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    val state = remoteConfig.getString("serviceState")
-
-                    if(state == "ON_SERVICE"){
-                        val intent = Intent(this, MainActivity::class.java)
-                        startActivity(intent)
-                    }
-                    else{
-                        binding.enterText.text = state
-                    }
-
-
-                    Log.d("seyoung", "state: $state")
-
-                } else {
-                    Log.d("seyoung","Fetch 실패")
-                }
+        viewModel.remoteConfigState.observe(this, Observer {
+            if(it){
+                Log.d("seyoung","intent : $it")
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+                finish()
             }
+        })
+
+        viewModel.remoteConfigMessage.observe(this, Observer {
+            binding.enterText.setText(it)
+        })
 
 
     }
