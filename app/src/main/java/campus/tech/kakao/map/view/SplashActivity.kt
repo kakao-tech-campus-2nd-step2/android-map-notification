eceb3e0c531db45ae9443cac84b9266fd97acfc6
LEEMINSEO <real2ms@naver.com>
2024-08-03 19:43:56 +0900

    feat: Push Notification

--- app/src/main/java/campus/tech/kakao/map/view/SplashActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/view/SplashActivity.kt
@@ -1,11 +1,19 @@
 package campus.tech.kakao.map.view
 
+import android.app.AlertDialog
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
+import campus.tech.kakao.map.R
 import campus.tech.kakao.map.viewmodel.SplashViewModel
 import campus.tech.kakao.map.databinding.ActivitySplashBinding
 import com.google.firebase.ktx.Firebase
@@ -17,6 +25,16 @@ class SplashActivity : AppCompatActivity() {
 
     private val viewModel: SplashViewModel by viewModels()
     private lateinit var binding: ActivitySplashBinding
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            viewModel.fetchRemoteConfig((Firebase.remoteConfig))
+        } else {
+            showPermissionDeniedMessage()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +49,8 @@ class SplashActivity : AppCompatActivity() {
 
         viewModel.fetchRemoteConfig(remoteConfig)
 
+        askNotificationPermission()
+
         viewModel.fetchSuccess.observe(this, Observer { success ->
             if (success) {
                 if (viewModel.serviceState.value == "ON_SERVICE") {
@@ -42,5 +62,51 @@ class SplashActivity : AppCompatActivity() {
                 }
             }
         })
+    }
+
+    private fun askNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.POST_NOTIFICATIONS
+                ) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                viewModel.fetchRemoteConfig((Firebase.remoteConfig))
+            } else if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
+                showNotificationPermissionDialog()
+            } else {
+                requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+    }
+
+    private fun showNotificationPermissionDialog() {
+        AlertDialog.Builder(this).apply {
+            setTitle(getString(R.string.ask_notification_permission_dialog_title))
+            setMessage(
+                String.format(
+                    "다양한 알림 소식을 받기 위해 권한을 허용하시겠어요?\n(알림 설정에서 %s의 알림 권한을 허용해주세요.)",
+                    getString(R.string.app_name)
+                )
+            )
+            setPositiveButton(getString(R.string.yes)) { _, _ ->
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package", packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            }
+            setNegativeButton(getString(R.string.no)) { _, _ -> }
+            show()
+        }
+    }
+
+    private fun showPermissionDeniedMessage() {
+        AlertDialog.Builder(this).apply {
+            setTitle(getString(R.string.permission_denied))
+            setMessage(getString(R.string.permission_denied_message))
+            setPositiveButton(getString(R.string.ok)) { _, _ -> }
+            show()
+        }
     }
 }
