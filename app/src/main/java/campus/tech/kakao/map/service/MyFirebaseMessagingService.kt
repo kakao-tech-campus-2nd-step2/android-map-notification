1e3f5a71ca65266e5bcf68cd8619da54bb8d8c04
LEEMINSEO <real2ms@naver.com>
2024-08-03 19:48:17 +0900

    refactor: change directory_MyFirebaseMessagingService.kt

--- app/src/main/java/campus/tech/kakao/map/service/MyFirebaseMessagingService.kt
+++ app/src/main/java/campus/tech/kakao/map/service/MyFirebaseMessagingService.kt
@@ -1,4 +1,4 @@
-package campus.tech.kakao.map
+package campus.tech.kakao.map.service
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
@@ -6,6 +6,7 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import androidx.core.app.NotificationCompat
+import campus.tech.kakao.map.R
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import campus.tech.kakao.map.view.SplashActivity
