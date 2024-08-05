33cfdd65204282bfbc3028630250259e66f56b70
jooiss <jusong513@pusan.ac.kr>
2024-08-05 11:50:34 +0900

    refactor: Move Remote config initialization to Application class

--- app/src/main/java/campus/tech/kakao/map/KakaoMapApplication.kt
+++ app/src/main/java/campus/tech/kakao/map/KakaoMapApplication.kt
@@ -3,17 +3,44 @@ package campus.tech.kakao.map
 import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import campus.tech.kakao.map.BuildConfig
+import android.util.Log
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.remoteConfigSettings
 import com.kakao.vectormap.KakaoMapSdk
 import dagger.hilt.android.HiltAndroidApp
+
 
 @HiltAndroidApp
 class KakaoMapApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        initRemoteConfig()
+        fetchRemoteConfig()
         createNotificationChannel()
         KakaoMapSdk.init(this, BuildConfig.KAKAO_API_KEY)
+    }
+
+    private fun initRemoteConfig() {
+        remoteConfig = FirebaseRemoteConfig.getInstance()
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 0
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
+    }
+
+    private fun fetchRemoteConfig() {
+        remoteConfig.fetchAndActivate().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val serviceState = remoteConfig.getString("serviceState")
+                val serviceMessage = remoteConfig.getString("serviceMessage")
+                Log.d("RemoteConfig", "Service State: $serviceState, Service Message: $serviceMessage")
+            }
+            else {
+                val e = task.exception
+                Log.e("RemoteConfig", "Fetch and activate failed", e)
+            }
+        }
     }
 
     private fun createNotificationChannel() {
@@ -26,6 +53,7 @@ class KakaoMapApplication : Application() {
     }
 
     companion object {
+        lateinit var remoteConfig: FirebaseRemoteConfig
         private const val NOTIFICATION_ID = 222222
         private const val CHANNEL_ID = "main_default_channel"
         private const val CHANNEL_NAME = "main channelName"
--- app/src/main/java/campus/tech/kakao/map/ui/SplashActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/ui/SplashActivity.kt
@@ -2,32 +2,41 @@ package campus.tech.kakao.map.ui
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import campus.tech.kakao.map.KakaoMapApplication
 import campus.tech.kakao.map.R
 import campus.tech.kakao.map.databinding.SplashLayoutBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
 
     private lateinit var splashBinding: SplashLayoutBinding
-    private val splashViewModel: SplashViewModel by viewModels()
+    private val splashDisplayLength: Long = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         splashBinding = DataBindingUtil.setContentView(this, R.layout.splash_layout)
 
-        splashViewModel.serviceState.observe(this) { serviceState ->
-            if (serviceState == "ON_SERVICE") {
-                val intent = Intent(this, MapActivity::class.java)
-                startActivity(intent)
-            }
-        }
+        val remoteConfig = KakaoMapApplication.remoteConfig
+        val serviceState = remoteConfig.getString("serviceState")
+        val serviceMessage = remoteConfig.getString("serviceMessage")
 
-        splashViewModel.serviceMessage.observe(this) { serviceMessage ->
-            splashBinding.tvError.text = serviceMessage
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(splashDisplayLength)
+            if (serviceState == "ON_SERVICE") {
+                val intent = Intent(this@SplashActivity, MapActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            else {
+                splashBinding.tvError.text = serviceMessage
+            }
         }
     }
 }
