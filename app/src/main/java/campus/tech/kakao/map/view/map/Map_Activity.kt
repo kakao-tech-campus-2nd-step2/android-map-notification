46951c192fe4a0707e7ede0f7bbf7e8ad370abc8
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-08-07 00:58:57 +0900

    message 추가

--- app/src/main/java/campus/tech/kakao/map/view/map/Map_Activity.kt
+++ app/src/main/java/campus/tech/kakao/map/view/map/Map_Activity.kt
@@ -8,70 +8,65 @@ import android.os.Bundle
 import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import campus.tech.kakao.map.R
 import campus.tech.kakao.map.service_message
 import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
-import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import com.kakao.vectormap.MapView
-
 
 class Map_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(campus.tech.kakao.map.R.layout.activity_map)
+        setContentView(R.layout.activity_map)
 
         val mapView = MapView(this)
-        val mapViewContainer = findViewById<FrameLayout>(campus.tech.kakao.map.R.id.map_view)
+        val mapViewContainer = findViewById<FrameLayout>(R.id.map_view)
         mapViewContainer.addView(mapView)
-        val mFirebaseRemoteConfig: FirebaseRemoteConfig = FirebaseRemoteConfig.getInstance()
-        val configSettings: FirebaseRemoteConfigSettings = Builder()
-            .setMinimumFetchIntervalInSeconds(3600)
-            .build()
-        mFirebaseRemoteConfig.setConfigSettingsAsync(configSettings)
 
+        val remoteConfig: FirebaseRemoteConfig = Firebase.remoteConfig
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 3600
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
 
-// 기본 값 설정
-        val defaultValues: MutableMap<String, Any> = HashMap()
-        defaultValues["serviceState"] = "ON_SERVICE"
-        defaultValues["serviceMessage"] = "서비스가 현재 중지되었습니다."
-        mFirebaseRemoteConfig.setDefaultsAsync(defaultValues)
+        // 기본 값 설정
+        val defaultValues: Map<String, Any> = mapOf(
+            "serviceState" to "ON_SERVICE",
+            "serviceMessage" to "서비스가 현재 중지되었습니다."
+        )
+        remoteConfig.setDefaultsAsync(defaultValues)
 
+        remoteConfig.fetchAndActivate()
+            .addOnCompleteListener(this, OnCompleteListener<Boolean> { task ->
+                if (task.isSuccessful) {
+                    val updated = task.result
+                    val serviceState: String = remoteConfig.getString("serviceState")
+                    val serviceMessage: String = remoteConfig.getString("serviceMessage")
 
-        mFirebaseRemoteConfig.fetchAndActivate()
-            .addOnCompleteListener(
-                this,
-                OnCompleteListener<Boolean> { task ->
-                    if (task.isSuccessful) {
-                        val updated = task.result
-                        val serviceState: String = mFirebaseRemoteConfig.getString("serviceState")
-                        val serviceMessage: String =
-                            mFirebaseRemoteConfig.getString("serviceMessage")
-
-                        // 서비스 상태
-                        if ("ON_SERVICE" == serviceState) {
-                            // 지도 화면으로 이동
-                            startActivity(
-                                Intent(
-                                    this, Map_Activity::class.java
-                                )
-                            )
-                            finish()
-                        } else {
-                            // service_message
-                            val intent = Intent(this, service_message::class.java)
-                            startActivity(intent)
-                        }
+                    // 서비스 상태
+                    if ("ON_SERVICE" == serviceState) {
+                        // 지도 화면으로 이동
+                        startActivity(Intent(this, Map_Activity::class.java))
+                        finish()
+                    } else {
+                        // service_message
+                        val intent = Intent(this, service_message::class.java)
+                        startActivity(intent)
                     }
-                })
+                }
+            })
 
-        //firebase message(FCM 사용)
+        // Firebase message (FCM 사용)
         class MyFirebaseMessagingService : FirebaseMessagingService() {
             override fun onMessageReceived(remoteMessage: RemoteMessage) {
                 if (remoteMessage.notification != null) {
-                    val title = remoteMessage.notification!!.title
-                    val message = remoteMessage.notification!!.body
+                    val title = remoteMessage.notification?.title
+                    val message = remoteMessage.notification?.body
                     sendCustomNotification(title, message)
                 }
             }
@@ -91,7 +86,7 @@ class Map_Activity : AppCompatActivity() {
                 }
 
                 val notificationBuilder: NotificationCompat.Builder =
-                    NotificationCompat.Builder(this, channelId)
+                    NotificationCompat.Builder(this@Map_Activity, channelId)
                         .setContentTitle(title)
                         .setContentText(message)
                         .setAutoCancel(true)
@@ -100,7 +95,6 @@ class Map_Activity : AppCompatActivity() {
                 notificationManager.notify(0, notificationBuilder.build())
             }
         }
-
-
     }
 }
+
